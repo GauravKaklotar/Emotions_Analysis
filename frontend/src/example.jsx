ea3bec9a5324e64ec8f516d0d79dd8809096d93c
GauravKaklotar <gaurav.kaklotar03@gmail.com>
2024-08-29 13:14:03 +0530

    Update frontend

--- frontend/src/example.jsx
+++ frontend/src/example.jsx
@@ -58,7 +58,7 @@ export default function Example() {
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Emotion Detection</span>
                 <img
-                  alt=""
+                  alt="Logo"
                   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                   className="h-8 w-auto"
                 />
